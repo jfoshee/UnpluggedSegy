eeaa22be319c4b4dee4bbfa3715f7355c357601d
jfoshee <jacobf@gmail.com>
2011-12-13 16:41:52 -0600

    First experiments in reading seg-y using the sample survey from RMOTC

--- Unplugged.Segy.Tests/SegyReaderTest.cs
+++ Unplugged.Segy.Tests/SegyReaderTest.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestDrivenDesign;
 
 namespace Unplugged.Segy.Tests
@@ -7,8 +11,44 @@ namespace Unplugged.Segy.Tests
     public class SegyReaderTest : TestBase<SegyReader>
     {
         [TestMethod]
-        public void Canary()
+        public void TryTextHeader()
         {
+            var path = @"C:\Users\jfoshee\Desktop\RMOTC Data\RMOTC Seismic data set\3D_Seismic\filt_mig.sgy";
+            using (var stream = File.OpenRead(path))
+            using (var reader = new BinaryReader(stream))
+            {
+                var header = reader.ReadBytes(3200);
+                Encoding ascii = Encoding.ASCII;
+                Encoding ebcdic = Encoding.GetEncoding("IBM037");
+                var headerAscii = Encoding.Convert(ebcdic, ascii, header);
+                var s = ascii.GetString(headerAscii);
+                Console.WriteLine(s);
+            }
+        }
+
+        [TestMethod]
+        public void TryTraceHeader()
+        {
+            var path = @"C:\Users\jfoshee\Desktop\RMOTC Data\RMOTC Seismic data set\3D_Seismic\filt_mig.sgy";
+            using (var stream = File.OpenRead(path))
+            using (var reader = new BinaryReader(stream))
+            {
+                reader.ReadBytes(3200);
+                reader.ReadBytes(400);
+                var header = reader.ReadBytes(240);
+
+                var inline = GetIntFromBytes(header, 17);
+                Assert.AreEqual(1, inline);
+                var crossline = GetIntFromBytes(header, 13);
+                Assert.AreEqual(1, crossline);
+            }
+        }
+
+        private static int GetIntFromBytes(byte[] header, int byteNumber)
+        {
+            var bytes = header.Skip(byteNumber - 1).Take(4).Reverse().ToArray();    // Reverse byte order big endian to little endian
+            var inline = BitConverter.ToInt32(bytes, 0);
+            return inline;
         }
     }
 }
