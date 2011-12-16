92fb32de3adab5d3914d7a1311af178e696b0c78
jfoshee <jacobf@gmail.com>
2011-12-16 17:25:28 -0600

    Finished the basic image writing for 2D!

--- Unplugged.Segy.Tests/ImageWriterTest.cs
+++ Unplugged.Segy.Tests/ImageWriterTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TestDrivenDesign;
@@ -24,5 +25,74 @@ namespace Unplugged.Segy.Tests
             Assert.AreEqual(1, image.Width);
             Assert.AreEqual(1, image.Height);
         }
+
+        [TestMethod]
+        public void ShouldHaveCorrectResolution()
+        {
+            // Arrange
+            var path = TestPath() + ".png";
+            var expectedWidth = 5;
+            var expectedHeight = 21;
+            var trace = new MockTrace { Values = new float[expectedHeight] };
+            var segy = new MockSegyFile { Traces = new ITrace[expectedWidth] };
+            for (int i = 0; i < expectedWidth; i++)
+                segy.Traces[i] = trace;
+
+            // Act
+            Subject.Write(segy, path);
+
+            // Assert
+            var image = Bitmap.FromFile(path);
+            Assert.AreEqual(expectedWidth, image.Width);
+            Assert.AreEqual(expectedHeight, image.Height);
+        }
+
+        [TestMethod]
+        public void PixelValuesShouldBeBlackToWhite()
+        {
+            // Arrange
+            var traceValues = new float[] { 150, 175, 125, 200, 111, 100, 123 };    // Min: 100, Max: 200
+            var trace = new MockTrace { Values = traceValues };
+            var segy = new MockSegyFile { Traces = new ITrace[] { trace } };
+            var path = TestPath() + ".png";
+
+            // Act
+            Subject.Write(segy, path);
+
+            // Assert
+            Bitmap image = new Bitmap(path);
+            Assert.AreEqual(Color.FromArgb(0, 0, 0), image.GetPixel(0, 5));
+            Assert.AreEqual(Color.FromArgb(127, 127, 127), image.GetPixel(0, 0));
+            Assert.AreEqual(Color.FromArgb(255, 255, 255), image.GetPixel(0, 3));
+        }
+
+        [TestMethod, DeploymentItem(@"Unplugged.Segy.Tests\Examples\lineE.sgy")]
+        public void ImageFromExample()
+        {
+            // Arrange
+            var segy = new SegyReader().Read("lineE.sgy");
+            var path = TestPath() + ".png";
+
+            // Act
+            Subject.Write(segy, path);
+
+            // Assert
+            TestContext.AddResultFile(path);
+        }
+
+        // TODO: Handle variable length traces
     }
+
+    class MockTrace : ITrace
+    {
+        public ITraceHeader Header { get; set; }
+        public IList<float> Values { get; set; }
+    }
+
+    class MockSegyFile : ISegyFile
+    {
+        public IFileHeader Header { get; set; }
+        public IList<ITrace> Traces { get; set; }
+    }
+
 }
--- Unplugged.Segy/ImageWriter.cs
+++ Unplugged.Segy/ImageWriter.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
 using System.Drawing;
 
 namespace Unplugged.Segy
@@ -7,8 +7,43 @@ namespace Unplugged.Segy
     {
         public void Write(ISegyFile segyFile, string path)
         {
-            var bitmap = new Bitmap(1, 1);
+            if (segyFile.Traces == null)
+            {
+                new Bitmap(1, 1).Save(path);
+                return;
+            }
+            var width = segyFile.Traces.Count;
+            var height = segyFile.Traces[0].Values.Count;
+            var bitmap = new Bitmap(width, height);
+            dynamic range = FindRange(segyFile.Traces);
+            var valueRange = range.Max - range.Min;
+            if (valueRange != 0)
+                AssignPixelColors(segyFile.Traces, width, height, bitmap, range.Min, valueRange);
             bitmap.Save(path);
+        }
+
+        private static void AssignPixelColors(IList<ITrace> traces, int width, int height, Bitmap bitmap, float valueMin, float valueRange)
+        {
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                {
+                    int byteValue = (int)(255 * (traces[i].Values[j] - valueMin) / valueRange);
+                    var color = Color.FromArgb(255, byteValue, byteValue, byteValue);
+                    bitmap.SetPixel(i, j, color);
+                }
+        }
+
+        private object FindRange(IList<ITrace> traces)
+        {
+            var min = float.MaxValue;
+            var max = float.MinValue;
+            foreach (var trace in traces)
+                foreach (var value in trace.Values)
+                {
+                    if (value < min) min = value;
+                    if (value > max) max = value;
+                }
+            return new { Min = min, Max = max };
         }
     }
 }
