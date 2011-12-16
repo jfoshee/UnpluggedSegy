fcf2908112411308cbfad55dcd356b80c56fc1ca
jfoshee <jacobf@gmail.com>
2011-12-15 19:15:28 -0600

    Started the ImageWriter which will save images from loaded SEG-Y files.

---
+++ Unplugged.Segy.Tests/ImageWriterTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestDrivenDesign;
+
+namespace Unplugged.Segy.Tests
+{
+    [TestClass]
+    public class ImageWriterTest : TestBase<ImageWriter>
+    {
+        [TestMethod]
+        public void ShouldCreateImageFile()
+        {
+            // Arrange
+            var path = TestPath() + ".png";
+            ISegyFile segyFile = new Mock<ISegyFile>().Object;
+
+            // Act
+            Subject.Write(segyFile, path);
+
+            // Assert
+            BinaryFileAssert.Exists(path);
+            var image = Bitmap.FromFile(path);
+            Assert.AreEqual(1, image.Width);
+            Assert.AreEqual(1, image.Height);
+        }
+    }
+}
---
+++ Unplugged.Segy/ImageWriter.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Drawing;
+
+namespace Unplugged.Segy
+{
+    public class ImageWriter
+    {
+        public void Write(ISegyFile segyFile, string path)
+        {
+            var bitmap = new Bitmap(1, 1);
+            bitmap.Save(path);
+        }
+    }
+}
