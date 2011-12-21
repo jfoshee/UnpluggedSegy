72a1f3b871e415b764b073e461a5b5a5d05f2288
jfoshee <jacobf@gmail.com>
2011-12-21 14:34:35 -0600

    Outstanding unit tests for reader extension methods

--- Unplugged.IbmBits.Tests/BinaryReaderExtensionMethodsTest.cs
+++ Unplugged.IbmBits.Tests/BinaryReaderExtensionMethodsTest.cs
@@ -7,46 +7,85 @@ namespace Unplugged.IbmBits.Tests
     [TestClass]
     public class BinaryReaderExtensionMethodsTest
     {
+        #region ReadStringEbcdic
+
+        [TestMethod]
+        public void ReadEbcdicShouldConsumeRequestedBytes()
+        {
+            var expected = 23;
+            AssertBytesConsumed(r => r.ReadStringEbcdic(expected), expected);
+        }
+
+        [TestMethod]
+        public void ShouldConvertCharacters()
+        {
+            VerifyValueFromByteStream("J", r => r.ReadStringEbcdic(1), new byte[] { 0xD1 });
+        }
+
+        #endregion
+
+        #region ReadInt16BigEndian()
+
+        [TestMethod]
+        public void Int16ShouldConsume2Bytes()
+        {
+            AssertBytesConsumed(r => r.ReadInt16BigEndian(), 2);
+        }
+
+        [TestMethod]
+        public void ShouldConvertToInt16()
+        {
+            VerifyValueFromByteStream(2, r => r.ReadInt16BigEndian(), new byte[] { 0, 2 });
+        }
+
+        #endregion
+
+        #region ReadInt32BigEndian()
+
+        [TestMethod]
+        public void Int32ShouldConsume4Bytes()
+        {
+            AssertBytesConsumed(r => r.ReadInt32BigEndian(), 4);
+        }
+
+        [TestMethod]
+        public void ShouldConvertToInt32()
+        {
+            VerifyValueFromByteStream(3, r => r.ReadInt32BigEndian(), new byte[] { 0, 0, 0, 3 });
+        }
+
+        #endregion
+
         #region ReadSingle()
 
         [TestMethod]
-        public void ShouldConsume4Bytes()
+        public void SingleShouldConsume4Bytes()
         {
             AssertBytesConsumed(r => r.ReadSingleIbm(), 4);
         }
 
         [TestMethod]
-        public void ZeroShouldBeTheSame()
+        public void ShouldConvertToSingle()
         {
-            float expected = 0.0f;
-            var bytes = BitConverter.GetBytes(expected);
-            VerifyReadSingleIbm(expected, bytes);
+            VerifyValueFromByteStream(1f, r => r.ReadSingleIbm(), new byte[] { 65, 16, 0, 0 });
         }
 
-        [TestMethod]
-        public void ShouldConvertFromIbmBitsExample()
-        {
-            var bytes = new byte[] { 0xc0, 0x1f, 0xf4, 0x62 };
-            var expected = -0.124822736f;
-            VerifyReadSingleIbm(expected, bytes);
-        }
+        #endregion
 
         // TODO: System.IO.EndOfStreamException: Unable to read beyond the end of the stream.
 
-        private static void VerifyReadSingleIbm(float expected, byte[] bytes)
+        private static void VerifyValueFromByteStream<T>(T expected, Func<BinaryReader, T> act, byte[] bytes)
         {
             using (var stream = new MemoryStream(bytes))
             using (var reader = new BinaryReader(stream))
             {
                 // Act
-                float actual = reader.ReadSingleIbm();
+                T actual = act(reader);
 
                 // Assert
                 Assert.AreEqual(expected, actual);
             }
         }
-
-        #endregion
 
         public static void AssertBytesConsumed(Action<BinaryReader> act, int expectedNumberOfBytes)
         {
