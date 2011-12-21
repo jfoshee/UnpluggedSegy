dd1cf838eb21799a586b24f99dfc61d3615a173e
jfoshee <jacobf@gmail.com>
2011-12-21 08:48:22 -0600

    Moving the IBM binary conversion stuff to its own project.

---
+++ Unplugged.IbmBits.Tests/IbmConverterTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDesign;
+
+namespace Unplugged.IbmBits.Tests
+{
+    [TestClass]
+    public class IbmConverterTest : TestBase<IbmConverter>
+    {
+        const byte _comma = 0x6B;
+        const byte _bang = 0x5A;
+        const byte _H = 0xC8;
+        const byte _e = 0x85;
+        const byte _l = 0x93;
+        const byte _o = 0x96;
+
+        [TestMethod]
+        public void ShouldConvertFromEbcdicToUnicode()
+        {
+            // Arrange
+            var bytes = new byte[] { _H, _e, _l, _l, _o, _comma };
+
+            // Act
+            string result = IbmConverter.ToString(bytes);
+
+            // Assert
+            Assert.AreEqual("Hello,", result);
+        }
+
+        [TestMethod]
+        public void ShouldConvertFromEbcdicToUnicode2()
+        {
+            // Arrange
+            var bytes = new byte[] { _l, _o, _l, _bang };
+
+            // Act
+            string result = IbmConverter.ToString(bytes);
+
+            // Assert
+            Assert.AreEqual("lol!", result);
+        }
+    }
+}
---
+++ Unplugged.IbmBits/IbmConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Unplugged.IbmBits
+{
+    public class IbmConverter
+    {
+        public static string ToString(byte[] bytes)
+        {
+            var unicode = Encoding.Unicode;
+            var ebcdic = Encoding.GetEncoding("IBM037");
+            var unicodeBytes = Encoding.Convert(ebcdic, unicode, bytes);
+            return unicode.GetString(unicodeBytes);
+        }
+    }
+}
