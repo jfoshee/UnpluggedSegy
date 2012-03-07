802a04f93e3fa4578fe23b50c72ca866e7a64f08
jfoshee <jacobf@gmail.com>
2012-03-07 16:30:40 -0600

    Added a yet-to-be-used interface for segy options

---
+++ Unplugged.Segy.Tests/SegyOptionsTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDesign;
+
+namespace Unplugged.Segy.Tests
+{
+    [TestClass]
+    public class SegyOptionsTest : TestBase<SegyOptions>
+    {
+        [TestMethod]
+        public void ImplementsInterface()
+        {
+            Assert.IsInstanceOfType(Subject, typeof(ISegyOptions));
+        }
+
+        [TestMethod]
+        public void DefaultValues()
+        {
+            Assert.AreEqual(true, Subject.IsEbcdic);
+            Assert.AreEqual(false, Subject.IsLittleEndian);
+
+            Assert.AreEqual(80, Subject.TextHeaderColumnCount);
+            Assert.AreEqual(40, Subject.TextHeaderRowCount);
+            Assert.AreEqual(true, Subject.TextHeaderInsertNewLines);
+
+            Assert.AreEqual(400, Subject.BinaryHeaderLength);
+            Assert.AreEqual(25, Subject.BinaryHeaderLocationForSampleFormat);
+
+            Assert.AreEqual(240, Subject.TraceHeaderLength);
+            Assert.AreEqual(115, Subject.TraceHeaderLocationForSampleCount);
+            Assert.AreEqual(189, Subject.TraceHeaderLocationForInlineNumber);
+            Assert.AreEqual(193, Subject.TraceHeaderLocationForCrosslineNumber);
+        }
+    }
+}
---
+++ Unplugged.Segy/SegyOptions.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Unplugged.Segy
+{
+    public class SegyOptions : ISegyOptions
+    {
+        public bool? IsEbcdic { get; set; }
+        public bool? IsLittleEndian { get; set; }
+
+        public int? TextHeaderColumnCount { get; set; }
+        public int? TextHeaderRowCount { get; set; }
+        public bool? TextHeaderInsertNewLines { get; set; }
+
+        public int? BinaryHeaderLength { get; set; }
+        public int? BinaryHeaderLocationForSampleFormat { get; set; }
+
+        public int? TraceHeaderLength { get; set; }
+        public int? TraceHeaderLocationForInlineNumber { get; set; }
+        public int? TraceHeaderLocationForCrosslineNumber { get; set; }
+        public int? TraceHeaderLocationForSampleCount { get; set; }
+
+        public SegyOptions()
+        {
+            IsEbcdic = true;
+            IsLittleEndian = false;
+            TextHeaderColumnCount = 80;
+            TextHeaderRowCount = 40;
+            TextHeaderInsertNewLines = true;
+            BinaryHeaderLength = 400;
+            BinaryHeaderLocationForSampleFormat = 25;
+            TraceHeaderLength = 240;
+            TraceHeaderLocationForSampleCount = 115;
+            TraceHeaderLocationForInlineNumber = 189;
+            TraceHeaderLocationForCrosslineNumber = 193;
+        }
+    }
+}
